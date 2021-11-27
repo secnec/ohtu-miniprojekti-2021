97afc1c87ac8e136ea9cd05c3326b243de2e5125
Katri <katri.rytkonen@outlook.com>
2021-11-27 15:53:34 +0200

    Registration and sign-in page templates created

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,22 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods=["get", "post"])
+def register():
+    if request.method=="GET":
+        return render_template("register.html")
+    
+    if request.method=="POST":
+        pass
+
+@app.route("/signin", methods=["get", "post"])
+def signin():
+    if request.method=="GET":
+        return render_template("signin.html")
+
+    if request.method=="POST":
+        pass
