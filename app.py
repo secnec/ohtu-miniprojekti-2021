db6d03b3b19fa0ac077d049922ec5a50cd1181bc
JuhahuJ <juha.lehtinen@helsinki.fi>
2021-11-28 16:34:13 +0200

    added database schema

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
 
 import src.routes
