386df18185ae66d582925c62ec91bad8804ec040
vaisest <4550061+vaisest@users.noreply.github.com>
2021-11-28 20:31:31 +0200

    add flask secret_key from .env

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from os import getenv
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL")
+# voidaan generoida esim python -c "import secrets; print(secrets.token_urlsafe(32))"
+# ja env tiedostossa SECRET_KEY=1A2B3B4B5BDFGKJ57F68
+app.config["SECRET_KEY"] = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
-
 import src.routes
