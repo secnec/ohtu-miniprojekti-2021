a96540d7b580487359d3176b11dba0d58d5ecdf2
vaisest <4550061+vaisest@users.noreply.github.com>
2021-12-01 18:04:59 +0200

    fix heroku postgres uri

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL").replace("postgres://", "postgresql://")
 # voidaan generoida esim python -c "import secrets; print(secrets.token_urlsafe(32))"
 # ja env tiedostossa SECRET_KEY=1A2B3B4B5BDFGKJ57F68
 app.config["SECRET_KEY"] = getenv("SECRET_KEY")
