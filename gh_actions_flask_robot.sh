16582009da7a4c913b87beeb52ee1945180fce3b
vaisest <4550061+vaisest@users.noreply.github.com>
2021-12-02 17:15:12 +0200

    try to debug environment variables

--- gh_actions_flask_robot.sh
+++ gh_actions_flask_robot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Github Actionsia varten, jottei robot testejä suoriteta Herokussa.
 apt-get update
+echo $SQLALCHEMY_DATABASE_URI
+echo $SECRET_KEY
 export SQLALCHEMY_DATABASE_URI
 export SECRET_KEY
 # apt-get install --yes postgresql-client #chromium-chromedriver
