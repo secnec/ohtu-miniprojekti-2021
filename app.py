454621c48594fdfb99088b5669181af264f96124
vaisest <4550061+vaisest@users.noreply.github.com>
2021-12-02 18:03:51 +0200

    remove environ, remove SQL warning, clean robot.sh

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import getenv, environ
+from os import getenv
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-print(environ)
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL").replace("postgres://", "postgresql://")
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("postgres://", "postgresql://")
 # voidaan generoida esim python -c "import secrets; print(secrets.token_urlsafe(32))"
 # ja env tiedostossa SECRET_KEY=1A2B3B4B5BDFGKJ57F68
 app.config["SECRET_KEY"] = getenv("SECRET_KEY")
--- gh_actions_flask_robot.sh
+++ gh_actions_flask_robot.sh
@@ -2,11 +2,14 @@
 # Tämä tiedosto on Github Actionsia varten,
 # jottei robot testejä suoriteta Herokussa.
 
+# Jotta aliprosessit näkevät muuttujat:
 export DATABASE_URL
 export SECRET_KEY
 
+# Lisää taulut tietokantaan
 psql -h localhost -U postgres < schema.sql
 
+# Flask suoritetaan taustalla
 poetry run flask run &
 
 sleep 30
