9a18aa6fedc7a2a92b59cffbe1ad87b037a15be8
vaisest <4550061+vaisest@users.noreply.github.com>
2021-12-02 17:52:04 +0200

    print environ

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import getenv
+from os import getenv, environ
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-print(os.environ)
+print(environ)
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL").replace("postgres://", "postgresql://")
 # voidaan generoida esim python -c "import secrets; print(secrets.token_urlsafe(32))"
 # ja env tiedostossa SECRET_KEY=1A2B3B4B5BDFGKJ57F68
