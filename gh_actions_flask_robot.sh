65e4577c26ad6c24d1dad261751bced0e548c8cf
vaisest <4550061+vaisest@users.noreply.github.com>
2021-12-02 17:44:10 +0200

    environs

--- gh_actions_flask_robot.sh
+++ gh_actions_flask_robot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Github Actionsia varten, jottei robot testejä suoriteta Herokussa.
 export SQLALCHEMY_DATABASE_URI=postgresql://postgres:postgres@localhost:5432
-export SECRET_KEY=TE$T_K€Y!
+export SECRET_KEY=TEsT_KeY
 # apt-get install --yes postgresql-client #chromium-chromedriver
 
 psql -h localhost -U postgres < schema.sql
 echo $SQLALCHEMY_DATABASE_URI
-poetry run flask run &
+SQLALCHEMY_DATABASE_URI=postgresql://postgres:postgres@localhost:5432 poetry run flask run &
 
 sleep 30
 
