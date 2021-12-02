d939aab1f4bc693cd0603d297691a00081626cbb
vaisest <4550061+vaisest@users.noreply.github.com>
2021-12-02 16:46:39 +0200

    test github actions postgres for tests

---
+++ gh_actions_flask_robot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Github Actionsia varten, jottei robot testejä suoriteta Herokussa.
+apt-get update
+apt-get install --yes postgresql-client #chromium-chromedriver
+
+psql -h localhost -U postgres < schema.sql
+
+poetry run flask run &
+
+sleep 30
+
+poetry run robot src/tests
