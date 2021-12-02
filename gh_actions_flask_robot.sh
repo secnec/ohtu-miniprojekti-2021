8ba27f65c66aa213dc3bcaf0045d8e61f40a9154
vaisest <4550061+vaisest@users.noreply.github.com>
2021-12-02 17:36:23 +0200

    remove apt-get update, debug echo

--- gh_actions_flask_robot.sh
+++ gh_actions_flask_robot.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # Github Actionsia varten, jottei robot testejä suoriteta Herokussa.
-apt-get update
 export SQLALCHEMY_DATABASE_URI=$1
 export SECRET_KEY=$2
 # apt-get install --yes postgresql-client #chromium-chromedriver
 
 psql -h localhost -U postgres < schema.sql
-
+echo $SQLALCHEMY_DATABASE_URI
 poetry run flask run &
 
 sleep 30
