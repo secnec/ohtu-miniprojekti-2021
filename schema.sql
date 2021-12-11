f8181186996b8256cc6a141c216db6ed7ed925e3
JuhahuJ <juha.lehtinen@helsinki.fi>
2021-12-11 13:56:43 +0200

    change database schema

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE TABLE users(id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
-CREATE TABLE tips(id SERIAL PRIMARY KEY, username TEXT, title TEXT, url TEXT, visible BOOLEAN DEFAULT True);
+CREATE TABLE tips(id SERIAL PRIMARY KEY, username TEXT, title TEXT, url TEXT, visible BOOLEAN DEFAULT True, likes INTEGER DEFAULT 0);
+CREATE TABLE likes(id SERIAL PRIMARY KEY, user_id INTEGER, tips_id INTEGER);
