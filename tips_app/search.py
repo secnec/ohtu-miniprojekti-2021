e61364b66ecc05466de5caef1d0852415ed1ba08
Natalie Johansson <mnrjohansson@gmail.com>
2021-12-15 11:31:15 +0200

    made search close matches ignore case

--- tips_app/search.py
+++ tips_app/search.py
@@ -23,8 +23,9 @@ def get_all_titles(tips):
     titles = list(map(get_title, tips))
     return titles
 
-def search_close_matches(tips, search_term):
+def search_close_matches(tips, search_term, maximum_search_results=50):
     """Returns a list of closely matching tip titles.
+        Case insensitive.
 
     Args:
         tips (list): A list of tips.
@@ -35,5 +36,27 @@ def search_close_matches(tips, search_term):
     Returns:
         [list]: A list of titles.
     """
-    tips_as_titles = get_all_titles(tips)
-    return get_close_matches(search_term, tips_as_titles, n=50)
+    search_term_lowercase = search_term.lower()
+    titles_lowercase_and_original = tip_dictionary(tips)
+    close_matches_lowercase = get_close_matches(search_term_lowercase,
+                             titles_lowercase_and_original.keys(),
+                             n=maximum_search_results)
+    matching_titles_original_case = [titles_lowercase_and_original[title] for title in close_matches_lowercase]
+    return matching_titles_original_case
+
+def tip_dictionary(tips):
+    """Creates a dictionary out of a tip list
+        with lowercase titles as its keys and
+        the original titles as the values.
+
+    Args:
+        tips ([list]): Unchanged tip list.
+
+    Returns:
+        [dict]: Keys: lowercase titles
+                values: original titles
+    """
+    titles = get_all_titles(tips)
+    dictionary = {titles[i].lower(): titles[i] for i in range(len(tips))}
+    return dictionary
+
